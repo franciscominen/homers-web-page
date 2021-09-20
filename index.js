a70209342ef1e46ed89e202c08b51da38ee102d4
Francisco Minen <fran.minen@gmail.com>
2021-09-20 15:42:38 -0300

    firts commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let sound = document.createElement('audio')
+sound.id = 'audio'
+sound.controls = ''
+sound.src = '/assets/backsound.mp3'
+sound.type = 'audio/mp3'
+document.body.appendChild(sound)
+
+function playAudio() {
+  document.getElementById('audio').play();
+}
+
+setTimeout("playAudio()", 2700);
+
+let mutedButton = document.getElementById('mutedBtn')
+
+mutedButton.onclick = function(e) {
+    sound.muted = true
+};
